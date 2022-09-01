a91061f8393c507c7620b66fd7790e3fb1640294
federico-palma <fedepal9@gmail.com>
2022-09-01 11:33:44 +0200

    Change to class object for list constructor

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import Main from "./Layout/Main";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-function TodoConstructor(title) {
-  this.id = uuidv4();
-  this.title = title;
-  this.todoArray = [];
-};
+class TodoListClass {
+  constructor(title) {
+    this.id = uuidv4();
+    this.title = title;
+    this.todoArray = ["do this", "and do this", "and also this"];
+  }
+
+  addNewTask(newTask) {
+    this.todoArray = [...this.todoArray, newTask]
+  }
+}
 
 function App() {
-
   const [todoLists, setTodoLists] = useState([]);
 
   const addNewListHandler = title => {
-    let newList = new TodoConstructor(title);
+    let newList = new TodoListClass(title);
 
     setTodoLists(prevState => [...prevState, newList]);
   };
+
+  // const updateListHandler = id => {
+  //   let newList
+
+  // }
 
   return (
     <div className="App">
